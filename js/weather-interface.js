0f705b97a30308b65da19431299370f1ec40a3d7
Chad Durkin and Melvin Gruschow <chad+melvin@>
2017-03-14 14:12:25 -0700

    finished

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,16 +1,14 @@
-var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
   var currentWeatherObject = new Weather();
-  currentWeatherObject.getWeather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-      $('.showTemperature').text("The temperature in " + city + " is " + (response.main.temp * (9/5) - 459.67));
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+    var humidity = currentWeatherObject.getWeather(city);
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
 });
